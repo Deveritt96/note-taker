3d9a54c3a12edc7bed7f14aefbae239558a25449
Devin <devin.everitt96@gmail.com>
2024-04-10 20:14:33 -0400

    started creating the backend files

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const notes = require('/note');
+
+PORT = 3001;
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Note-Taker app listening at http://localhost:${PORT}`);
+  });
+  
