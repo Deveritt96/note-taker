26a1da518d1bc243fcaba08a92aac7d58f9d8258
Devin <devin.everitt96@gmail.com>
2024-04-22 19:43:38 -0400

    added the code to GET from database

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,15 @@
 const fs = require ('fs');
 const path = require('path');
 const router = require =('express').Router();
+const dbFilePath = path.join(__dirname, '../db/db.json');
+
+router.get('/notes' (req, res) => {
+    fs.readFile(dbFilePath. 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ error: 'Internal server error'});
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
